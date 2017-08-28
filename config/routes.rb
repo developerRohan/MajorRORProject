ba02399e37ab49f39d4c38f0926d414fd4500144
MY RAILS <jhambrohan44@gmail.com>
2017-08-28 20:46:38 +0530

    added seeds file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'uses/name:string'
 
   resources :posts
   devise_for :users
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+20.times do |post|
+	Post.create(date: Date.today , rationale: "#{post} rationale content" , adjustment: "abc" ,Department: "ECE" , count: 1)
+end
+
+puts "20 posts have benn created"
