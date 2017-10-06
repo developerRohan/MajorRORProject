6af2e818c8d9b96ab0d02e4ead74661de63bbaff
MY RAILS <jhambrohan44@gmail.com>
2017-10-07 00:35:32 +0530

    added resource generator for posts and added some tests in post_spec.rb

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,26 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  describe "Creation" do
+	describe "creation" do
+		before do
+			@post = Post.create(date: Date.today , rationale:"anything");
+		end
 
-  	before do
-      @user = User.create(email: "rohan@gmail.com" , password: "rohanarora" , password_confirmation: "rohanarora" , first_name: "rohan" , last_name: "arora")
-      login_as(@user , :scope => :user)
-  		@post = Post.create(date: Date.today , rationale: "anything" , adjustment: "anybody" , Department: "ECE" , count: 1 , user_id: @user.id);
-  	end
+		it "can be craeted" do
+			expect(@post).to be_valid
+		end
 
-  	it 'can be created' do
-  		expect(@post).to be_valid
-  	end
+		it "cannot be created without dates and rationale" do
+			@post.date = nil;
+			@post.rationale = nil;
+			expect(@post).to_not be_valid
+		end
 
-
-  	it "cannot be created" do
-  		@post.date = nil
-  		@post.rationale = nil
-  		@post.adjustment = nil
-  		@post.Department = nil
-  		
-  		expect(@post).to_not be_valid
-  	end
-  end
+	end
 end
