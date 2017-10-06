a04c5498c7f794f9ac9a9fa85af858bb6eff8d84
MY RAILS <jhambrohan44@gmail.com>
2017-10-07 03:16:01 +0530

    builded a relational connection between users and posts

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -17,8 +17,11 @@ describe 'navigate' do
    describe 'creation' do
 
     before do
+      user = User.create(first_name:"rohan" , last_name:"arora" ,email:"rohan@gmail.com" , password:"rohanarora" , password_confirmation:"rohanarora");
+      login_as(user , :scope => :user)
       visit new_post_path
     end
+
 	  it 'a new post can be created' do
   		expect(page.status_code).to eq(200)
     end
@@ -29,6 +32,13 @@ describe 'navigate' do
      click_on "Save"
      expect(page).to have_content("some rationale")
     
+    end
+
+    it 'will have user associated with it' do
+     fill_in 'post[date]', with: Date.today
+     fill_in 'post[rationale]', with: "User Association"
+     click_on "Save"
+     expect(User.last.posts.last.rationale).to eq("User Association")
     end
    	
    end
