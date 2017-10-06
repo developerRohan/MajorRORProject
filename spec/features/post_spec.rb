88eaaeb7ff3bbaa782362ca431db4cf6c7069cf4
MY RAILS <jhambrohan44@gmail.com>
2017-10-07 02:30:33 +0530

    added BDD behavior for creating posts

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -7,10 +7,28 @@ describe 'navigate' do
   		expect(page.status_code).to eq(200)
 	end
 
-	it 'has a title of Posts' do
-		visit posts_path
-		expect(page).to have_content(/Posts/)
+	# it 'has a title of Posts' do
+	# 	visit posts_path
+	# 	expect(page).to have_content(/Posts/)
 
-	end
+	# end
    end
+
+   describe 'creation' do
+	it 'a new post can be created' do
+		visit new_post_path
+		expect(page.status_code).to eq(200)
+   	end
+
+   	it 'can be created from new form page' do
+     visit new_post_path
+	   fill_in 'post[date]', with: Date.today
+	   fill_in 'post[rationale]', with: "some rationale"
+     click_on "Save"
+     expect(page).to have_content("some rationale")
+    
+    end
+   	
+   end
+
 end
