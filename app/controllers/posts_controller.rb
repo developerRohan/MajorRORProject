765cdb248a73f592904d5667074873050282ba9f
MY RAILS <jhambrohan44@gmail.com>
2017-10-10 17:21:17 +0530

    addded post policy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,12 +20,12 @@ class PostsController < ApplicationController
   	end
 
   	def edit
-
+      authorize @post
   	end
 
   	def update
-
-  	if @post.update(post_params)
+       authorize @post
+    if @post.update(post_params)
   		redirect_to @post, notice: 'Your post was updated successfully'
   	else
   		render :edit
